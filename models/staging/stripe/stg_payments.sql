34045ad4a44b096178ff3b4bb4c3ff9a5348320c
Christine Berger <christine@dbtlabs.com>
2024-10-09 07:36:12 +0000

    Added column to stg_payments

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ select
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-
+        1 as fun,
         --`amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount
 
