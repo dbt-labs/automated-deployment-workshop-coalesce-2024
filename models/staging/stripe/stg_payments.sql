13d955091d01553f337e85e005e7bc15ec3842c6
Sankalp Vatsh <sankalp.vatsh3@dbtlabscoalesce.com>
2024-10-10 22:10:49 +0000

    Test change

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,8 +3,8 @@ select
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-
+        
         --`amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
+        amount as amount
 
 from {{ source('stripe', 'payment') }}
